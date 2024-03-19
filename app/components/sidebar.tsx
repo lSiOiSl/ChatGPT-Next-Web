b595cc2e026c9c47e72985d40abf7bc5b0fda193
ibosswangzj9 <ibosswangzj9123456>
2024-03-19 12:57:54 +0800

    修改展示文案

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,17 +155,17 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+        我的聊天窗口
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        允许创建多个聊天窗口
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
       </div>
 
-      <div className={styles["sidebar-header-bar"]}>
+      {/* <div className={styles["sidebar-header-bar"]}>
         <IconButton
           icon={<MaskIcon />}
           text={shouldNarrow ? undefined : Locale.Mask.Name}
@@ -186,7 +186,7 @@ export function SideBar(props: { className?: string }) {
           onClick={() => showToast(Locale.WIP)}
           shadow
         />
-      </div>
+      </div> */}
 
       <div
         className={styles["sidebar-body"]}
